4bdbc120c942ce1dffe20e5dc653bcc97baed065
Gary-Dutton <training@a4artificial-i4intelligence.com>
2022-10-01 04:17:57 +0100

    Amend variables for public/private use

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // create a variable to store a default float value
-    public float speed = 1.0f;
+    [SerializeField]
+    private float _speed = 1.0f;
 
 
     // Start is called before the first frame update
@@ -25,8 +26,8 @@ public class Player : MonoBehaviour
         float horitonalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.right * horitonalInput * Time.deltaTime * speed); // Time.deltaTime normalises the computer, by using realtime
-        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed); // without T.dT the movement on different computer would be sporadic 
+        transform.Translate(Vector3.right * horitonalInput * Time.deltaTime * _speed); // Time.deltaTime normalises the computer, by using realtime
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * _speed); // without T.dT the movement on different computer would be sporadic 
 
     }
 }
